98d1ac3b2d816aae5d6cd471f31b456ebbebe1b6
Chen Chen <chen.alexander.chen@gmail.com>
2021-03-14 15:49:02 -0400

    Formatted cpu.rs

--- nes_emulator/src/cpu.rs
+++ nes_emulator/src/cpu.rs
@@ -1,32 +1,31 @@
 /**
  * Simulator of 6502.
- * 
+ *
  * For 6502 instruction references, see http://www.obelisk.me.uk/6502/reference.html and http://www.6502.org/tutorials/6502opcodes.html
  */
 
 // OPCODEs.
-const OPCODE_BRK : u8 = 0x00;
-const OPCODE_INX : u8 = 0xe8;
-const OPCODE_LDA_IMMEDIATE : u8 = 0xa9;
-const OPCODE_TAX : u8 = 0xaa;
+const OPCODE_BRK: u8 = 0x00;
+const OPCODE_INX: u8 = 0xe8;
+const OPCODE_LDA_IMMEDIATE: u8 = 0xa9;
+const OPCODE_TAX: u8 = 0xaa;
 
 // Status masks.
 // Note that we only have 7 status registers for 8 bits of "process status" register.
 // Bit 5 is always set to 1. Since nothing can change it, it is of no use to programmers.
 //
 // See https://www.atarimagazines.com/compute/issue53/047_1_All_About_The_Status_Register.php
-const STATUS_MASK_CARRY_FLAG : u8 = 0b0000_0001;  // C bit: bit 0
-const STATUS_MASK_ZERO_FLAG: u8 = 0b0000_0010;  // Z bit: bit 1
-const STATUS_MASK_INTERRUPT_DISABLE : u8 = 0b0000_0100;  // I bit: bit 2
-const STATUS_MASK_DECIMAL_MODE : u8 = 0b0000_1000;  // D bit: bit 3
-const STATUS_MASK_BREAK_COMMAND : u8 = 0b0001_0000;  // B bit: bit 4
-const STATUS_MASK_OVERFLOW_FLAG : u8 = 0b0100_0000;  // V bit: bit 6
-const STATUS_MASK_NEGATIVE_FLAG : u8 = 0b1000_0000;  // N bit: bit 7
+const STATUS_MASK_CARRY_FLAG: u8 = 0b0000_0001; // C bit: bit 0
+const STATUS_MASK_ZERO_FLAG: u8 = 0b0000_0010; // Z bit: bit 1
+const STATUS_MASK_INTERRUPT_DISABLE: u8 = 0b0000_0100; // I bit: bit 2
+const STATUS_MASK_DECIMAL_MODE: u8 = 0b0000_1000; // D bit: bit 3
+const STATUS_MASK_BREAK_COMMAND: u8 = 0b0001_0000; // B bit: bit 4
+const STATUS_MASK_OVERFLOW_FLAG: u8 = 0b0100_0000; // V bit: bit 6
+const STATUS_MASK_NEGATIVE_FLAG: u8 = 0b1000_0000; // N bit: bit 7
 
 // Initial register values.
 // Bit 5 in the status register is always set to 1.
-const DEFAULT_STATUS_REGISTER : u8 = 0b0001_0000;  // A default 
-
+const DEFAULT_STATUS_REGISTER: u8 = 0b0001_0000; // A default
 
 pub struct CPU {
     pub reg_a: u8,      // register A.
@@ -78,9 +77,9 @@ impl CPU {
     }
 
     // Sets the N bit of status register based on the value of |register|.
-    fn set_negative_flag(&mut self, register : u8) {
+    fn set_negative_flag(&mut self, register: u8) {
         if register & 0b1000_0000 == 0 {
-            self.reg_status &= !STATUS_MASK_NEGATIVE_FLAG; 
+            self.reg_status &= !STATUS_MASK_NEGATIVE_FLAG;
         } else {
             self.reg_status |= STATUS_MASK_NEGATIVE_FLAG;
         }
@@ -115,13 +114,10 @@ impl CPU {
     // Handles instruction TAX.
     fn tax(&mut self) {
         self.reg_x = self.reg_a;
-                    
+
         self.set_negative_flag(self.reg_x);
         self.set_zero_flag(self.reg_x);
     }
-
-
-    
 }
 
 #[cfg(test)]
@@ -131,7 +127,7 @@ mod test {
     #[test]
     fn test_initial_register() {
         let cpu = CPU::new();
-    
+
         assert_eq!(cpu.pc, 0);
         assert_eq!(cpu.reg_a, 0);
         assert_eq!(cpu.reg_status, 0b0001_0000);
@@ -227,7 +223,7 @@ mod test {
         cpu.reg_x = 0x0f;
         // INX
         let program = vec![0xe8, 0x00];
-        
+
         cpu.interpret(&program);
 
         assert_eq!(cpu.pc, 2);
@@ -241,12 +237,12 @@ mod test {
         cpu.reg_x = 0xff;
         // INX
         let program = vec![0xe8, 0x00];
-        
+
         cpu.interpret(&program);
 
         assert_eq!(cpu.pc, 2);
         assert_eq!(cpu.reg_x, 0x00);
-        assert_eq!(cpu.reg_status, 0b0001_0010);       
+        assert_eq!(cpu.reg_status, 0b0001_0010);
     }
 
     #[test]
@@ -255,12 +251,12 @@ mod test {
         cpu.reg_x = 0xe0;
         // INX
         let program = vec![0xe8, 0x00];
-        
+
         cpu.interpret(&program);
 
         assert_eq!(cpu.pc, 2);
         assert_eq!(cpu.reg_x, 0xe1);
-        assert_eq!(cpu.reg_status, 0b1001_0000);       
+        assert_eq!(cpu.reg_status, 0b1001_0000);
     }
 
     #[test]
